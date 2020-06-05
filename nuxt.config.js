fe8fc0cc29808e2305a7f91ad616b53b7f838ba7
venkat <subash.venkat007@gmail.com>
2020-06-05 12:57:50 +0530

    chore(nuxt.config.js pages/dashboard.vue): firebase userflow

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/firestore.js', '~/plugins/authpersistense'],
+  plugins: ['~/plugins/firestore.js', '~/plugins/authPersistense.js'],
   /*
    ** Nuxt.js dev-modules
    */
