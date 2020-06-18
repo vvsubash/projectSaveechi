88614f964a204c7b7ca834b2c11428b676872d1a
vvsubash <subash.venkat007@gmail.com>
2020-06-18 10:44:20 +0530

    feat(addcow.vue): data to be added when adding cow, WIP

--- pages/user/cows/addCow.vue
+++ pages/user/cows/addCow.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
+    <label for="newCow">Name of the Animal</label>
     <input id="newCow" v-model="newCow" type="text" name="newCow" />
+    <label for="state">What state is she is in</label>
+    <select id="state" v-model="cowStateEntered" name="state">
+      <option v-for="cowState in possibleCowStates" :key="cowState">{{
+        cowState
+      }}</option>
+    </select>
+    <label for="dateOfRecentCalving">Date of recent calving</label>
+    <input v-model="dateOfRecentCalvingEntered" type="date" />
+    <input
+      id="wasSheInseminated"
+      v-model="wasSheInseminated"
+      type="checkbox"
+      name="wasSheInseminated"
+    />
+    <label for="wasSheInseminated">Was She Inseminated</label>
+    <br />
     <button @click="addCow">Add Cow</button>
   </div>
 </template>
@@ -11,7 +28,17 @@ import db from '~/plugins/firestore'
 export default {
   data() {
     return {
-      newCow: null
+      newCow: null,
+      cowType: null,
+      possibleCowStates: [
+        'justCalved',
+        'canBeInseminated',
+        'insseminated',
+        'dry'
+      ],
+      cowStateEntered: null,
+      dateOfRecentCalvingEntered: null,
+      wasSheInseminated: true
     }
   },
   methods: {
@@ -20,7 +47,10 @@ export default {
       db.collection(`users/${this.$store.state.user.uid}/cows`)
         .doc(newCow)
         .set({
-          name: this.newCow
+          name: this.newCow,
+          state: this.cowStateEntered,
+          dateOfRecentCalving: new Date(this.dateOfRecentCalvingEntered),
+          wasSheInseminated: this.wasSheInseminated
         })
     }
   },
