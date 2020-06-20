a45052d897970e11a5d73ba17408a9a77a93a697
vvsubash <subash.venkat007@gmail.com>
2020-06-20 10:06:52 +0530

    chore(index.vue): removed unnecessary links

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,13 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        projectSaveechi
-      </h1>
-      <h2 class="subtitle">
-        My majestic Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-        <!-- TODO need to refactor -->
-        <button @click="signIn">Login</button>
-      </div>
-    </div>
+    <!-- TODO need to refactor -->
+    <button @click="signIn">Login</button>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  },
+  components: {},
   methods: {
     signIn() {
       this.$store.dispatch('signIn')
