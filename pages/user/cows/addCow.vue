b80dbbf0db8ee14b1a10bd6e02f188ac634c9d4f
venkat <subash.venkat007@gmail.com>
2020-07-27 11:57:11 +0530

    chore(editcow addcoe): added checkbox for no purpose editCow

--- pages/user/cows/addCow.vue
+++ pages/user/cows/addCow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <!-- <div>
     <label for="newCow">Name of the Animal</label>
     <input id="newCow" v-model="newCow" type="text" name="newCow" />
     <label for="state">What state is she is in</label>
@@ -20,6 +20,57 @@
     {{ whenCanSheBeInseminated }}
     <br />
     <button @click="addCow">Add Cow</button>
+  </div> -->
+  <div>
+    <v-stepper v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step :complete="e1 > 1" step="1"
+          >Name of step 1</v-stepper-step
+        >
+
+        <v-divider></v-divider>
+
+        <v-stepper-step :complete="e1 > 2" step="2"
+          >Name of step 2</v-stepper-step
+        >
+
+        <v-divider></v-divider>
+
+        <v-stepper-step step="3">Name of step 3</v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 2">
+            Continue
+          </v-btn>
+
+          <v-btn text>Cancel</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="2">
+          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 3">
+            Continue
+          </v-btn>
+
+          <v-btn text>Cancel</v-btn>
+        </v-stepper-content>
+
+        <v-stepper-content step="3">
+          <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+          <v-btn color="primary" @click="e1 = 1">
+            Continue
+          </v-btn>
+
+          <v-btn text>Cancel</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
   </div>
 </template>
 
